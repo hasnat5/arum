0907d29281b0ff47edaebc626a12d55679488e03
hasnat5 <hasnatferdiananda5@gmail.com>
2022-12-04 22:53:37 +0700

    add accesibility and change header

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ const footer = () => {
             <div className="grid gap-3">
                 <div className="flex gap-3 items-center">
                     <img src={logo} className="h-6" alt="logo" />
-                    <h2 className="text-third font-tungsten text-3xl tracking-wider">ARUM</h2>
+                    <h1 className="text-third font-tungsten text-3xl tracking-wider">ARUM</h1>
                 </div>
                 <p className="text-third font-din text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error et, architecto ullam excepturi eveniet dolor!</p>
             </div>
@@ -17,8 +17,8 @@ const footer = () => {
             <div className="text-third">
                 <h2 className="font-dinBold mb-4">SOSIAL MEDIA</h2>
                 <ul className="font-din text-fourth flex gap-3">
-                    <li><a href="https://www.instagram.com/pplgtwooo_/" target="_blank"><img src={instagram} alt="" /></a></li>
-                    <li><a href="https://www.youtube.com/@pplgtwoofus9054" target="_blank"><img src={youtube} alt="" /></a></li>
+                    <li><a href="https://www.instagram.com/pplgtwooo_/" target="_blank"><img src={instagram} alt="instagram-logo" /></a></li>
+                    <li><a href="https://www.youtube.com/@pplgtwoofus9054" target="_blank"><img src={youtube} alt="youtube-logo" /></a></li>
                 </ul>
             </div>
 
