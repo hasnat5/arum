341ed35a98cc2091e5fa9c9ff9e7ebb63379f3e0
hasnat5 <hasnatferdiananda5@gmail.com>
2022-12-04 22:17:21 +0700

    adding style

--- src/App.js
+++ src/App.js
@@ -6,20 +6,27 @@ import Aktor from './components/Aktor';
 import About from "./components/About";
 import Navbar from './components/Navbar';
 import Footer from "./components/Footer";
+import NotFound from './components/NotFound';
 import { Fragment } from "react";
 
 function App() {
-  return (
-    <Fragment>
-      <Navbar></Navbar>
-      <Routes className="App">
-        <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/aktor' element={<Aktor></Aktor>}></Route>
-        <Route path='/about' element={<About></About>}></Route>
-      </Routes>
-      <Footer></Footer>
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<Navbar></Navbar>
+			<Routes className="App">
+				<Route path='/' element={<Home />} />
+
+				<Route path='/actor'>
+					<Route index element={<Aktor />} />
+				</Route>
+
+				<Route path='/about' element={<About />} />
+
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+			<Footer></Footer>
+		</Fragment>
+	);
 }
 
 export default App;
