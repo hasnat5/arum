874afd6aff98595698815ed941e76a2374d92634
hasnat5 <hasnatferdiananda5@gmail.com>
2022-12-05 18:41:53 +0700

    add padding top for navbar

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import tentang from "../asset/img/jumbotron.jpg";
 
 const about = () => {
     return (
-        <section className="bg-third">
+        <section className="bg-third pt-16">
             <div className="px-6 py-12">
                 <h1 className="text-secondary font-bold font-tungsten text-6xl mb-9">ABOUT US</h1>
                 <p className="font-din text-lg text-fourth">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero officiis consectetur nemo nihil accusamus natus et quo autem at suscipit tenetur atque amet laudantium totam tempore, eveniet placeat id mollitia optio itaque? Sapiente fugiat vero quisquam nihil? Consequatur, reiciendis necessitatibus!</p>
--- src/components/Aktor.jsx
+++ src/components/Aktor.jsx
@@ -3,7 +3,7 @@ import React from "react"
 
 const aktor = () => {
     return (
-        <section className="bg-green-500">
+        <section className="bg-green-500 pt-16">
             <h1>Aktor</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, rem eum mollitia ratione temporibus cum est quos esse vel tempore sint, aut voluptas quo ullam repellat velit, possimus at neque.</p>
         </section>
