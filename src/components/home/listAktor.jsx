797baaf527f4f6dfcf99d851b2acfd41c008203b
hasnat5 <hasnatferdiananda5@gmail.com>
2022-12-07 16:16:19 +0700

    change image

--- src/components/home/listAktor.jsx
+++ src/components/home/listAktor.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Aktor from './../../asset/img/hasnat.png';
+import Aktor from './../../asset/img/anel.png';
 
 const listAktor = () => {
     return (
