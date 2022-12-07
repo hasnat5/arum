db36140bd89ae1588f115748276b618609e93b04
hasnat5 <hasnatferdiananda5@gmail.com>
2022-12-07 16:00:26 +0700

    add thumbnail

--- src/components/Aktor.jsx
+++ src/components/Aktor.jsx
@@ -7,7 +7,7 @@ const aktor = () => {
         <section className="bg-green-500 pt-16">
             <h1>Aktor</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, rem eum mollitia ratione temporibus cum est quos esse vel tempore sint, aut voluptas quo ullam repellat velit, possimus at neque.</p>
-            <img src={Aktor} alt="" />
+            <img src={Aktor} alt="poto_aktor" />
         </section>
     )
 }
