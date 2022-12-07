c35175a4f40c7ab0b1949a4299e3b65423ab2a88
hasnat5 <hasnatferdiananda5@gmail.com>
2022-12-07 14:27:02 +0700

    change nav to indo language

--- src/components/navigation/mobileNavigation.jsx
+++ src/components/navigation/mobileNavigation.jsx
@@ -4,10 +4,10 @@ import "../navigation/mobileNavigation.css";
 
 const nav = (props) => {
     return (
-        <ul id="navbar" className="navbar absolute grid gap-4 content-center justify-center top-0 left-0 bg-secondary font-tungsten text-4xl text-white w-screen h-screen">
-            <li onClick={() => props.isMobile && props.closeMobileMenu()}><NavLink to="/">Home</NavLink></li>
+        <ul id="navbar" className="navbar absolute grid gap-4 content-center justify-center text-center top-0 left-0 bg-secondary font-tungsten text-4xl text-white w-screen h-screen">
+            <li onClick={() => props.isMobile && props.closeMobileMenu()}><NavLink to="/">Beranda</NavLink></li>
             <li onClick={() => props.isMobile && props.closeMobileMenu()}><NavLink to="/actor">Aktor</NavLink></li>
-            <li onClick={() => props.isMobile && props.closeMobileMenu()}><NavLink to="/about">About</NavLink></li>
+            <li onClick={() => props.isMobile && props.closeMobileMenu()}><NavLink to="/about">Tentang</NavLink></li>
         </ul>
     )
 }
