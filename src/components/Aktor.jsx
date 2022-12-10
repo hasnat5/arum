dcf7fc9101ba0a9c79f0c2fd477ae31fc4540a99
hasnat5 <hasnatferdiananda5@gmail.com>
2022-12-10 13:10:26 +0700

    fix responsive on actor menu

--- src/components/Aktor.jsx
+++ src/components/Aktor.jsx
@@ -30,7 +30,7 @@ const people = [
     { "nama": "Wulan Shavira Nopa", "peran": "Ibu Arum", "img": ibuArum, "deskripsi": "seorang ibu yang memiliki mempunyai sifat sabar dan lemah lembut ia begitu tunduk kepada suami dengan ketegaran hatinya ibu mampu sabar menghadapi bapa yang berkeras hati kepada arum" },
     { "nama": "Marwa Aulia Lukman", "peran": "Qiran", "img": Qiran, "deskripsi": "Qiran adalah salah satu sahabat sejati arum ia senantiasa menyemangati arum ditengah keterpurukan arum, ia berusaha menjaga arum untuk tetap tersenyum" },
     { "nama": "Sherryl Azizah Aulia", "peran": "Ola", "img": Ola, "deskripsi": "Ola adalah salah satu sahabat sejati arum, ia terkenal di sekolah karena kecantikannya, karena itupun ia menjadi pusat perhatian di kelasnya" },
-    { "nama": "Bisma Gyndara Mages Jayalangit", "peran": "Ayah Gyndara", "img": ayahGyndara, "deskripsi": "Seorang ayah yang tidak ingin anaknya menderita seperti apa yang ia alami" },
+    { "nama": "Bisma Gyndara M . J", "peran": "Ayah Gyndara", "img": ayahGyndara, "deskripsi": "Seorang ayah yang tidak ingin anaknya menderita seperti apa yang ia alami" },
     { "nama": "Darrel Rafa Raihan", "peran": "Ayah Brandon", "img": ayahBrandon, "deskripsi": "Dia adalah seorang juragan sapi yang sangat kaya di kampungnya ia berwibawa tinggi untuk memperjuangkan keinginan seorang anaknya" },
     { "nama": "Nadilla Nur Afifah", "peran": "Ibu Brandon", "img": ibuBrandon, "deskripsi": "Ia sangat mendukung anaknya untuk mendapatkan apa yang anaknya inginkan" },
     { "nama": "Ikhsan Rifansyah", "peran": "Brandon", "img": Brandon, "deskripsi": "Brandon anak dari juragan sapi di kampungnya, dia seorang anak yang ketergantungan kepada orang tua nya" },
@@ -53,7 +53,8 @@ const ListAktor = () => {
         <section className="container m-auto min-w-full pt-16" >
             <div className="grid justify-center pt-6 bg-secondary lg:grid-cols-12 lg:px-16 lg:pt-10">
 
-                <div className="w-64 lg:grid lg:col-span-3 lg:content-center">
+                <div className="lg:grid lg:col-span-3 lg:content-start">
+                    <h1 className="font-tungsten text-6xl lg:text-8xl text-third mb-12 hidden lg:block">{selected.nama}</h1>
                     <h2 className="font-dinBold text-xl mb-4 text-third hidden lg:block">PILIH AKTOR</h2>
                     <Listbox className="hidden lg:block" value={selected} onChange={setSelected}>
                         <div className="relative mt-1">
@@ -72,7 +73,7 @@ const ListAktor = () => {
                                 leaveFrom="opacity-100"
                                 leaveTo="opacity-0"
                             >
-                                <Listbox.Options className="absolute mt-1 max-h-60 w-64 overflow-auto rounded-md bg-third py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                                <Listbox.Options className="absolute mt-1 max-h-60 w-72 overflow-auto rounded-md bg-third py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
                                     {people.map((person, personIdx) => (
                                         <Listbox.Option
                                             key={personIdx}
@@ -101,8 +102,8 @@ const ListAktor = () => {
                     </Listbox>
                 </div>
 
-                <h1 className="absolute lg:max-w-sm left-0 right-0 mx-auto font-tungsten text-6xl lg:text-8xl text-third text-center">{selected.nama}</h1>
-                <img src={selected.img} className="h-[70vh] z-10 relative grid justify-self-center lg:col-start-4 lg:col-span-6" alt="poto_aktor" />
+                <h1 className="absolute lg:max-w-sm left-0 right-0 mx-auto font-tungsten text-6xl lg:text-8xl text-third text-center lg:hidden">{selected.nama}</h1>
+                <img src={selected.img} className="h-[70vh] z-10 relative grid justify-self-center lg:col-start-4 lg:col-span-6 mt-20 lg:mt-10" alt="poto_aktor" />
 
                 <article className="hidden lg:grid lg:content-center lg:col-start-10 lg:col-span-3">
                     <div className="lg:block text-third">
@@ -126,55 +127,56 @@ const ListAktor = () => {
                         <h2 className="font-dinBold text-xl text-fourth mb-4">DESKRIPSI</h2>
                         <p className="font-din text-fourth text-lg">{selected.deskripsi}</p>
 
-                        <div className="grid justify-items-center">
-                            <div className="min-w-full">
-                                <Listbox value={selected} onChange={setSelected}>
-                                    <div className="relative mt-1">
-                                        <Listbox.Button className="relative w-full cursor-default border border-fourth bg-third py-2 pl-3 pr-10 text-center font-dinBold text-secondary uppercase focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-                                            <span className="block truncate">{selected.peran}</span>
-                                            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                                                <ChevronUpDownIcon
-                                                    className="h-5 w-5 text-gray-400"
-                                                    aria-hidden="true"
-                                                />
-                                            </span>
-                                        </Listbox.Button>
-                                        <Transition
-                                            as={Fragment}
-                                            leave="transition ease-in duration-100"
-                                            leaveFrom="opacity-100"
-                                            leaveTo="opacity-0"
-                                        >
-                                            <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-third py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                                                {people.map((person, personIdx) => (
-                                                    <Listbox.Option
-                                                        key={personIdx}
-                                                        className={({ active }) =>
-                                                            `relative cursor-default select-none py-2 px-6 ${active ? 'bg-secondary text-third uppercase' : 'text-fourth font-din uppercase'
-                                                            }`
-                                                        }
-                                                        value={person}
-                                                    >
-                                                        {({ selected }) => (
-                                                            <>
-                                                                <span
-                                                                    className={`block truncate ${selected ? 'font-medium' : 'font-normal'
-                                                                        }`}
-                                                                >
-                                                                    {person.peran}
-                                                                </span>
-                                                            </>
-
-                                                        )}
-                                                    </Listbox.Option>
-                                                ))}
-                                            </Listbox.Options>
-                                        </Transition>
-                                    </div>
-                                </Listbox>
-                            </div>
-                        </div>
                     </article>
+
+                    <div className="grid justify-items-center">
+                        <div className="min-w-full">
+                            <Listbox value={selected} onChange={setSelected}>
+                                <div className="relative mt-1">
+                                    <Listbox.Button className="relative w-full cursor-default border border-fourth bg-third py-2 pl-3 pr-10 text-center font-dinBold text-secondary uppercase focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+                                        <span className="block truncate">{selected.peran}</span>
+                                        <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                                            <ChevronUpDownIcon
+                                                className="h-5 w-5 text-gray-400"
+                                                aria-hidden="true"
+                                            />
+                                        </span>
+                                    </Listbox.Button>
+                                    <Transition
+                                        as={Fragment}
+                                        leave="transition ease-in duration-100"
+                                        leaveFrom="opacity-100"
+                                        leaveTo="opacity-0"
+                                    >
+                                        <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-third py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                                            {people.map((person, personIdx) => (
+                                                <Listbox.Option
+                                                    key={personIdx}
+                                                    className={({ active }) =>
+                                                        `relative cursor-default select-none py-2 px-6 ${active ? 'bg-secondary text-third uppercase' : 'text-fourth font-din uppercase'
+                                                        }`
+                                                    }
+                                                    value={person}
+                                                >
+                                                    {({ selected }) => (
+                                                        <>
+                                                            <span
+                                                                className={`block truncate ${selected ? 'font-medium' : 'font-normal'
+                                                                    }`}
+                                                            >
+                                                                {person.peran}
+                                                            </span>
+                                                        </>
+
+                                                    )}
+                                                </Listbox.Option>
+                                            ))}
+                                        </Listbox.Options>
+                                    </Transition>
+                                </div>
+                            </Listbox>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
