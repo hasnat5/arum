074cb760783f6f4f78f56e38c9a8e5cec179c9b8
hasnat5 <hasnatferdiananda5@gmail.com>
2022-12-12 01:16:25 +0700

    change hasnat photo

--- src/components/about/aboutUs.jsx
+++ src/components/about/aboutUs.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import hasnatDev from "../../asset/img/fadhli_pengembang.jpg";
+import hasnatDev from "../../asset/img/hasnat_pengembang.jpg";
 import fadhliDev from "../../asset/img/fadhli_pengembang.jpg";
 
 const aboutUs = () => {
